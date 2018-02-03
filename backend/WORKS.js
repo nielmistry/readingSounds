34880ed544d757cfdfe739661b6ffb7e29bfe7d5
Brendan J <johnstonbrendan111@gmail.com>
2018-02-03 13:17:24 -0500

    Make it work properly

--- backend/WORKS.js
+++ backend/WORKS.js
@@ -1,28 +1,30 @@
+//ony works for  some;
 var gluten_info;
-var gluten_link;
-var chapterGiven;
 var book_wanted;
-var chapter_wanted_minus_1 = 11;//put input here
+var chapter_wanted = 3;
+var chapter_wanted_minus_1 = chapter_wanted - 1;//put input here
 var text_link;
-var toneID;
 var scan_chapter = 0;
-var fs = require('fs')
+var fs = require('fs');
 var data = '';
 //below reads  in the json and ouputs the link to text file.
 //below did not word due to the json file having 'text/plain; charset=us-ascii'
-
-function gluten_to_text (name){
-  var fs = require('fs');
-    var contents = fs.readFileSync('http://gutendex.com/books/?search=' + name)//may jhave to change the file name
-    var jsonContent = JSON.parse(contents);
-    text_link = jsonContent.results[0].formats['text/plain; charset=us-ascii'];
-  };
-
+function gluten_to_text (){
+  var contents = fs.readFileSync('test.json'); //this one is for a test using alice
+  //var contents = fs.readFileSync('gluten_info.json')//may jhave to change the file name
+  var jsonContent = JSON.parse(contents);
+  var search_index = 0;
+  do {
+  text_link = jsonContent.results[search_index].formats['text/plain; charset=us-ascii'];
+  search_index++;
+} while (text_link.indexOf(".txt") == -1);
+  // start = string_chapter.indexOf("CHAPTER");
+};
+gluten_to_text();
 
 //below takes the link and downloads it to directory
 function link_to_file(){
   var http = require('http');
-  var fs = require('fs');
 
   var file = fs.createWriteStream("gluten_info.txt");
   var request = http.get(text_link, function(response) {
@@ -31,7 +33,7 @@ function link_to_file(){
   });
 };
 
-gluten_to_text("hi");
+link_to_file();
 
 //below reads in chunk by chunk and only the desired chapter
 function read_to_desired(){
@@ -46,9 +48,7 @@ function read_to_desired(){
     start = string_chapter.indexOf("CHAPTER");
     string_chapter = string_chapter.replace(/CHAPTER/, "");
     end = string_chapter.indexOf("CHAPTER");
-    scan_
-
-chapter++;
+    scan_chapter++;
   }
   if (end < 0)
   {
@@ -62,49 +62,67 @@ chapter++;
   write_to_file();
 };
 //below takes in the data of the chapter and makes it into a text read
-//to later be  converted into a  json
+//to later be  converted into a json
 function write_to_file(){
-  var fs = require('fs');
   var input = fs.writeFileSync('input_chapter.txt', data,'utf-8');
   console.log("Done!!");
+  //console.log(data);
+  white_space_begone();
+  JSONify();
 };
-
-
-var done;
-JSONify();
-
-function JSONify(){
-	var data = fs.readFileSync('input_chapter.txt','utf-8');
-	var dataNew = data.replace(/"/g,'\\"');
- dataNew = dataNew.replace(/[^\x00-\x7F]/g,'');
-dataNew = dataNew.replace(/\x2F/g,"");
- dataNew = dataNew.replace(/\n/g,'');
-  newdata = '{\n  \"text\": \" ' + dataNew + '\"\n}';
-  fs.writeFileSync('output.json',newdata,'utf-8');
-  done = fs.readFileSync('output.json','utf-8');
-  console.log("JSONIFY DONE");
+//HERE NOT COMPLETE,INVISIBLE WHITE SPACE ZERO WIDTH THINGS IN FILE
+function white_space_begone()
+{ //put in a while loop that looks and returns the index of returns similar to the chapter ONE
+  // then everytime it finds a return just delete twice to either get rid of the enter or
+  // remove the enter and delete twice per amount
+  //try to do 32 and up in terms of what is being recognised.
+  // data = data.replace(/\n/g,' ');
+  var white_s = data.slice(6400,6600);
+  // data = data.replace(white_s,'')
+  // white_s = data.slice(15,16)
+  // data = data.replace(white_s, '')
+  // white_s = data.slice(15,16)
+  console.log(white_s + " this has a char value of: ");
+  // console.log(white_s.charCodeAt(0));
 }
 
-console.log(done);
+function JSONify(){
+  var dataNew = data.replace(/"/g,'\\"');
+  dataNew = dataNew.replace(/[^\x00-\x7F]/g,'');
+  dataNew = dataNew.replace(/\n/g,' ');
+  dataNew = dataNew.replace(/\r/g,'');
+  dataNew = dataNew.replace(/\f/g,'');
+  //THIS ONE IS ACTUALLY ABLE TO REMOVE ONE OF THEM dataNew = dataNew.replace(/(^[\s\u200b]*|[\s\u200b]*$)/g,'');
+  newdata = '{\n  \"text\": \" ' + dataNew + '\"\n}';
+  fs.writeFileSync('Chapter_1.json',newdata);
+  done = fs.readFileSync('Chapter_1.json');
+  console.log("JSONIFY DONE");
+  console.log("done")
+  amazingAI();
+}
+
+console.log("done");
 var response;
+
 function amazingAI(){
+  console.log("in amazing AI");
   var ToneAnalyserV3 = require('watson-developer-cloud/tone-analyzer/v3');
   var tone_analyser = new ToneAnalyserV3({
     username: '859b35a5-f631-4683-9ba3-52add7d63165',
-          password: 'hpHkNrMGSeOe',//remove before git (maybe)
-          version_date: '2017-09-21'
-        });
-
+    password: 'hpHkNrMGSeOe',//remove before git (maybe)
+    version_date: '2017-09-21'
+  });
+  console.log("did it get this far");
   var params = {
-    'tone_input' : require('./output.json'),
+    'tone_input' : require('./Chapter_1.json'),
     'content_type' : 'application/json',
     'sentences' : false
   };
 
-
+  console.log("right before chapter_1 json params");
   tone_analyser.tone(params, function(error, response){
     if (error)
-      console.log('error:', error);
+    console.log('error:', error);
     else
     {
       console.log(JSON.stringify(response,null,2));
@@ -116,79 +134,26 @@ function amazingAI(){
 
 function saveJSON(resp)
 {
-	var jsonfile = require('jsonfile');
-	var file = './response.json';
-	jsonfile.writeFileSync(file,resp)
-//	get_values();
+  var jsonfile = require('jsonfile');
+  var file = 'response.json';
+  jsonfile.writeFileSync(file,resp)
+  //	get_values();
 };
-
+var jsonContent
 function get_values()
 {
-  var fs = require("fs");
   var contents = fs.readFileSync("response.json");
-  var jsonContent = JSON.parse(contents);
+  jsonContent = JSON.parse(contents);
   var max = 0;
   var max_index = -1;
   for(iterator = 0; iterator < jsonContent.document_tone.tones.length; iterator++)
   {
     console.log("this is the something value", jsonContent.document_tone.tones[iterator].score);
     if (max < jsonContent.document_tone.tones[iterator].score)
+    {
       max = jsonContent.document_tone.tones[iterator].score;
-    max_index = iterator;
-  }
+      max_index = iterator;
+    }
+  };
   console.log("The type of music I want is", jsonContent.document_tone.tones[max_index].tone_id)
-  toneID = jsonContent.document_tone.tones[max_index].tone_id;
-};
-
-amazingAI();
-
-var http = require('http');
-var express = require('express');
-var app = express();
-
-app.get('/', function(req, res) {
-  res.send('Hello World!');
-});
-
-app.get('/data/search/:name-:chapter', function(req, res) {
-  //gluten_to_text(name);
-  chapter_wanted_minus_1 = chapter - 1;
-  res.send({"url": sendURL(req.params.name),"tone" : toneID});
-});
-
-app.listen(32401, function() {
-  console.log('Example app listening on port 1337!');
-});
-
-
-//gluten_to_text("hi");
-
-
-function sendURL(bookName)
-{
-	if(bookName === "Alice in Wonderland")
-	{
-   return 'http:\/\/www.gutenberg.org\/ebooks\/11.epub.noimages';
- }
- else if(bookName === "My Man Jeeves")
- {
-   return 'http:\/\/www.gutenberg.org\/ebooks\/8164.epub.images';
- }
- else
- {
-   return 'google.com';
- }
 }
-
-/*
-
-http.createServer((request, response) => {
-  response.setHeader('Access-Control-Allow-Origin', '*');
-  let body = [];
-  request.on('data', (chunk) => {
-    body.push(chunk);
-  }).on('end', () => {
-    body = Buffer.concat(body).toString();
-    response.end(body);
-  });
-}).listen(1337); */
--- backend/book_to_chapter_to_mood.js
+++ backend/book_to_chapter_to_mood.js
@@ -1,37 +1,33 @@
 var gluten_info;
-var gluten_link;
-var chapterGiven;
 var book_wanted;
 var chapter_wanted_minus_1 = 11;//put input here
 var text_link;
-var toneID;
 var scan_chapter = 0;
 var fs = require('fs')
 var data = '';
 //below reads  in the json and ouputs the link to text file.
 //below did not word due to the json file having 'text/plain; charset=us-ascii'
-
-function gluten_to_text (name){
-  var fs = require('fs');
-    var contents = fs.readFileSync('http://gutendex.com/books/?search=' + name)//may jhave to change the file name
+function gluten_to_text (){
+    var fs = require('fs');
+    var contents = fs.readFileSync('gluten_info.json')//may jhave to change the file name
     var jsonContent = JSON.parse(contents);
     text_link = jsonContent.results[0].formats['text/plain; charset=us-ascii'];
-  };
-
+};
+gluten_to_text();
 
 //below takes the link and downloads it to directory
 function link_to_file(){
   var http = require('http');
   var fs = require('fs');
 
-  var file = fs.createWriteStream("gluten_info.txt");
+ var file = fs.createWriteStream("gluten_info.txt");
   var request = http.get(text_link, function(response) {
     var stream = response.pipe(file);
     stream.on('finish',function () {read_to_desired()});
   });
 };
 
-gluten_to_text("hi");
+link_to_file();
 
 //below reads in chunk by chunk and only the desired chapter
 function read_to_desired(){
@@ -46,9 +42,7 @@ function read_to_desired(){
     start = string_chapter.indexOf("CHAPTER");
     string_chapter = string_chapter.replace(/CHAPTER/, "");
     end = string_chapter.indexOf("CHAPTER");
-    scan_
-
-chapter++;
+    scan_chapter++;
   }
   if (end < 0)
   {
@@ -62,7 +56,7 @@ chapter++;
   write_to_file();
 };
 //below takes in the data of the chapter and makes it into a text read
-//to later be  converted into a  json
+//to later be  converted into a json
 function write_to_file(){
   var fs = require('fs');
   var input = fs.writeFileSync('input_chapter.txt', data,'utf-8');
@@ -70,49 +64,48 @@ function write_to_file(){
 };
 
 
-var done;
+//------------------------------------------
 JSONify();
 
 function JSONify(){
-	var data = fs.readFileSync('input_chapter.txt','utf-8');
+	var data = fs.readFileSync('input_chapter.txt');
 	var dataNew = data.replace(/"/g,'\\"');
- dataNew = dataNew.replace(/[^\x00-\x7F]/g,'');
-dataNew = dataNew.replace(/\x2F/g,"");
- dataNew = dataNew.replace(/\n/g,'');
-  newdata = '{\n  \"text\": \" ' + dataNew + '\"\n}';
-  fs.writeFileSync('output.json',newdata,'utf-8');
-  done = fs.readFileSync('output.json','utf-8');
-  console.log("JSONIFY DONE");
+	 dataNew = dataNew.replace(/[^\x00-\x7F]/g,'');
+	dataNew = dataNew.replace(/\n/g,'');
+	newdata = '{\n  \"text\": \" ' + dataNew + '\"\n}';
+	fs.writeFileSync('Chapter_1.json',newdata);
+	done = fs.readFileSync('Chapter_1.json');
+	console.log("JSONIFY DONE");
 }
-
+/*
 console.log(done);
 var response;
 function amazingAI(){
-  var ToneAnalyserV3 = require('watson-developer-cloud/tone-analyzer/v3');
-  var tone_analyser = new ToneAnalyserV3({
-    username: '859b35a5-f631-4683-9ba3-52add7d63165',
+        var ToneAnalyserV3 = require('watson-developer-cloud/tone-analyzer/v3');
+        var tone_analyser = new ToneAnalyserV3({
+          username: '859b35a5-f631-4683-9ba3-52add7d63165',
           password: 'hpHkNrMGSeOe',//remove before git (maybe)
           version_date: '2017-09-21'
         });
 
-  var params = {
-    'tone_input' : require('./output.json'),
-    'content_type' : 'application/json',
-    'sentences' : false
-  };
+        var params = {
+          'tone_input' : require('./Chapter_1.json'),
+          'content_type' : 'application/json',
+	'sentences' : false
+        };
 
 
-  tone_analyser.tone(params, function(error, response){
-    if (error)
-      console.log('error:', error);
-    else
-    {
-      console.log(JSON.stringify(response,null,2));
-      saveJSON(response);
-      get_values();
-    }
-  });
-};
+        tone_analyser.tone(params, function(error, response){
+          if (error)
+          console.log('error:', error);
+          else
+	{
+            console.log(JSON.stringify(response,null,2));
+		saveJSON(response);
+		get_values();
+	}
+        });
+      };
 
 function saveJSON(resp)
 {
@@ -130,65 +123,14 @@ function get_values()
   var max = 0;
   var max_index = -1;
   for(iterator = 0; iterator < jsonContent.document_tone.tones.length; iterator++)
-  {
+
     console.log("this is the something value", jsonContent.document_tone.tones[iterator].score);
     if (max < jsonContent.document_tone.tones[iterator].score)
       max = jsonContent.document_tone.tones[iterator].score;
     max_index = iterator;
   }
   console.log("The type of music I want is", jsonContent.document_tone.tones[max_index].tone_id)
-  toneID = jsonContent.document_tone.tones[max_index].tone_id;
 };
 
 amazingAI();
-
-var http = require('http');
-var express = require('express');
-var app = express();
-
-app.get('/', function(req, res) {
-  res.send('Hello World!');
-});
-
-app.get('/data/search/:name-:chapter', function(req, res) {
-  //gluten_to_text(name);
-  chapter_wanted_minus_1 = chapter - 1;
-  res.send({"url": sendURL(req.params.name),"tone" : toneID});
-});
-
-app.listen(32401, function() {
-  console.log('Example app listening on port 1337!');
-});
-
-
-//gluten_to_text("hi");
-
-
-function sendURL(bookName)
-{
-	if(bookName === "Alice in Wonderland")
-	{
-   return 'http:\/\/www.gutenberg.org\/ebooks\/11.epub.noimages';
- }
- else if(bookName === "My Man Jeeves")
- {
-   return 'http:\/\/www.gutenberg.org\/ebooks\/8164.epub.images';
- }
- else
- {
-   return 'google.com';
- }
-}
-
-/*
-
-http.createServer((request, response) => {
-  response.setHeader('Access-Control-Allow-Origin', '*');
-  let body = [];
-  request.on('data', (chunk) => {
-    body.push(chunk);
-  }).on('end', () => {
-    body = Buffer.concat(body).toString();
-    response.end(body);
-  });
-}).listen(1337); */
+//get_values();
